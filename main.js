f8c12cd89a62653cfdd65c45be6e2375a7accc44
MK <mortezakarim0@gmail.com>
2022-10-12 19:08:04 +0100

    js file added

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 const quoteText = document.querySelector(".quote"),
-authorName = document.querySelector(".author .name"),
+    authorName = document.querySelector(".author .name"),
     quoteBtn = document.querySelector("buttons");
 
 
 
-function randomQuote(){
-    fetch("https://api.quotable.io/random").then(res => res.json()).then(result =>{
+function randomQuote() {
+    fetch("https://api.quotable.io/random").then(res => res.json()).then(result => {
         console.log(result)
         quoteText.innerText = result.content;
         authorName.innerText = result.author;
     })
 
-} 
+}
 
 quoteBtn.addEventListener("click", randomQuote);
